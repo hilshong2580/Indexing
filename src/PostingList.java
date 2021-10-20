daf6646a226a165c05d60897121b8ff62ef614ac
hilshong2580 <63128458+hilshong2580@users.noreply.github.com>
2021-10-20 12:39:31 -0400

    Update PostingList.java
    add more function in postingList

--- src/PostingList.java
+++ src/PostingList.java
@@ -1,2 +1,51 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class PostingList {
+    private List<Posting> postings = new ArrayList<>();
+    private int index;
+
+    public PostingList(){
+        this.postings = new ArrayList<>();
+        index = 0;
+    }
+    public boolean haveMore(){
+        return postings.size() > index;
+    }
+
+    public Posting getCurPosting(){
+        if(postings.size()==0)
+            return null;
+        return this.postings.get(this.index);
+    }
+
+    public Posting getIndexPosting(int index){
+        return this.postings.get(index);
+    }
+
+    public void skipTo(int docid){
+        for(int i = 0; i< postings.size(); i++)
+            if(postings.get(i).getDocId() >= docid){
+                this.index = i;
+                break;
+            }
+    }
+
+    public List<Posting> getPostings(){
+        return this.postings;
+    }
+
+    public void addPosting(Posting p){
+        this.postings.add(p);
+        this.index++;
+    }
+
+    public int getCurListMaxDocId(){
+        Posting posting = postings.get(postings.size() - 1 );
+       return posting.getDocId();
+    }
+
+    public void setIndex(){
+        this.index = 0;
+    }
 }
